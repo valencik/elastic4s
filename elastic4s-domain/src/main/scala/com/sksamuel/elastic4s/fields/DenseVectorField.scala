566d7d4298a715718a7db09b497dfb0a116f3199
Andrew Valencik <andrew.valencik@shopify.com>
2022-12-07 14:15:58 -0500

    Add support for indexing DenseVectorField

--- elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/DenseVectorField.scala
+++ elastic4s-domain/src/main/scala/com/sksamuel/elastic4s/fields/DenseVectorField.scala
@@ -4,6 +4,8 @@ object DenseVectorField {
   val `type`: String = "dense_vector"
 }
 case class DenseVectorField(name: String,
-                            dims: Int) extends ElasticField {
+                            dims: Int,
+                            index: Option[Boolean] = None, // https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-index.html
+                            similarity: Option[String] = None) extends ElasticField {
   override def `type`: String = DenseVectorField.`type`
 }
